95780d8c3ea5b4ecd27769da5e8ffde3f42de78a
Raghu Kaippully <rkaippully@gmail.com>
2022-07-04 08:15:49 +0530

    Use a simple nix flake that builds on multiple GHC versions

--- flake.nix
+++ flake.nix
@@ -2,46 +2,68 @@
   description = "WebGear Project";
 
   inputs = {
-    # nixos-22.05 on 2022-06-25
-    nixpkgs.url = "github:NixOS/nixpkgs/ccf8bdf72624521358be6bb7d9b524c4cbcf7aff";
-    # master on 2022-06-25
-    flake-utils.url = "github:numtide/flake-utils/bee6a7250dd1b01844a2de7e02e4df7d8a0a206c";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
   };
 
   outputs = { self, nixpkgs, flake-utils }:
-    flake-utils.lib.eachDefaultSystem (system:
+    flake-utils.lib.eachSystem ["x86_64-linux" "x86_64-darwin"] (system:
       let
+        mapcat = f: lst: builtins.foldl' (l: r: l // r) {} (map f lst);
+
         pkgs = nixpkgs.legacyPackages.${system};
 
-        haskellPackages = pkgs.haskell.packages.ghc922.override {
-          overrides = final: prev: {
-            bytestring-conversion = final.callPackage ./nix/haskell-modules/bytestring-conversion-0.3.2.nix {};
+        hsVersions = ["ghc923" "ghc902" "ghc8107"];
+        hsDefaultVersion = "ghc923";
+
+        haskellPackages = mapcat (hsVersion: {
+          ${hsVersion} = pkgs.haskell.packages.${hsVersion}.override {
+            # Add any package overrides here
+            # overrides = final: prev: {
+            #  package-name = final.callPackage ./nix/haskell-modules/package-name-version.nix {};
+            # };
           };
-        };
+        }) hsVersions;
+
+        localPackages = hsVersion:
+          let
+            hsPkgs = haskellPackages.${hsVersion};
+            webgear-core = hsPkgs.callCabal2nix "webgear-core" ./webgear-core {};
+          in {
+            "webgear-core-${hsVersion}" = webgear-core;
+            "webgear-server-${hsVersion}" = hsPkgs.callCabal2nix "webgear-server" ./webgear-server {
+              inherit webgear-core;
+            };
+            "webgear-openapi-${hsVersion}" = hsPkgs.callCabal2nix "webgear-openapi" ./webgear-openapi {
+              inherit webgear-core;
+            };
+          };
+
+        mkDevShell = hsVersion:
+          let hsPkgs = haskellPackages.${hsVersion};
+              shell = hsPkgs.shellFor {
+                name = "webgear-dev-${hsVersion}";
+                doBenchmark = true;
+
+                packages = p: map (name: p.${name})
+                  ["webgear-core" "webgear-server" "webgear-openapi"];
+
+                buildInputs = [
+                  hsPkgs.ghc
+                  haskellPackages.${hsDefaultVersion}.fourmolu
+                  pkgs.cabal-install
+                  pkgs.hlint
+                  pkgs.haskell-language-server
+                  # (haskellPackages.${hsDefaultversion}.packdeps
+                  pkgs.stack
+                ];
+
+                inputsFrom = builtins.attrValues self.packages.${system};
+              };
+          in { ${shell.name} = shell; };
       in {
-        packages = rec {
-          webgear-core = haskellPackages.callCabal2nix "webgear-core" ./webgear-core {};
+        packages = mapcat localPackages hsVersions;
 
-          webgear-server = haskellPackages.callCabal2nix "webgear-server" ./webgear-server {
-            inherit webgear-core;
-          };
-
-          webgear-openapi = haskellPackages.callCabal2nix "webgear-openapi" ./webgear-openapi {
-            inherit webgear-core;
-          };
-        };
-
-        devShell = pkgs.mkShell {
-          buildInputs = with pkgs; [
-            cabal2nix
-            cabal-install
-            ghcid
-            haskellPackages.haskell-language-server
-            # haskellPackages.packdeps
-            stack
-          ];
-
-          inputsFrom = builtins.attrValues self.packages.${system};
-        };
+        devShells = mapcat mkDevShell hsVersions;
       });
 }
