6be1507a8e80d54dbf047989f2790f07500186f4
Raghu Kaippully <rkaippully@gmail.com>
2022-08-24 11:47:21 +0530

    Refactoring

--- flake.nix
+++ flake.nix
@@ -4,66 +4,32 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
     flake-utils.url = "github:numtide/flake-utils";
+    gitignore = {
+      url = "github:hercules-ci/gitignore.nix";
+      # Use the same nixpkgs
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
-  outputs = { self, nixpkgs, flake-utils }:
-    flake-utils.lib.eachSystem ["x86_64-linux" "x86_64-darwin"] (system:
+  outputs = { self, nixpkgs, flake-utils, gitignore }:
+    {
+      overlays.default = import ./nix/overlays/haskell.nix { inherit gitignore; };
+    }
+    // flake-utils.lib.eachSystem ["x86_64-linux" "x86_64-darwin"] (system:
       let
-        mapcat = f: lst: builtins.foldl' (l: r: l // r) {} (map f lst);
+        pkgs = import nixpkgs {
+          inherit system;
+          overlays = [ self.overlays.default ];
+        };
 
-        pkgs = nixpkgs.legacyPackages.${system};
-
-        hsVersions = ["ghc923" "ghc902" "ghc8107"];
-        hsDefaultVersion = "ghc923";
-
-        haskellPackages = mapcat (hsVersion: {
-          ${hsVersion} = pkgs.haskell.packages.${hsVersion}.override {
-            # Add any package overrides here
-            # overrides = final: prev: {
-            #  package-name = final.callPackage ./nix/haskell-modules/package-name-version.nix {};
-            # };
-          };
-        }) hsVersions;
-
-        localPackages = hsVersion:
-          let
-            hsPkgs = haskellPackages.${hsVersion};
-            webgear-core = hsPkgs.callCabal2nix "webgear-core" ./webgear-core {};
-          in {
-            "webgear-core-${hsVersion}" = webgear-core;
-            "webgear-server-${hsVersion}" = hsPkgs.callCabal2nix "webgear-server" ./webgear-server {
-              inherit webgear-core;
-            };
-            "webgear-openapi-${hsVersion}" = hsPkgs.callCabal2nix "webgear-openapi" ./webgear-openapi {
-              inherit webgear-core;
-            };
-          };
-
-        mkDevShell = hsVersion:
-          let hsPkgs = haskellPackages.${hsVersion};
-              shell = hsPkgs.shellFor {
-                name = "webgear-dev-${hsVersion}";
-                doBenchmark = true;
-
-                packages = p: map (name: p.${name})
-                  ["webgear-core" "webgear-server" "webgear-openapi"];
-
-                buildInputs = [
-                  hsPkgs.ghc
-                  haskellPackages.${hsDefaultVersion}.fourmolu
-                  pkgs.cabal-install
-                  pkgs.hlint
-                  pkgs.haskell-language-server
-                  # (haskellPackages.${hsDefaultversion}.packdeps
-                  pkgs.stack
-                ];
-
-                inputsFrom = builtins.attrValues self.packages.${system};
-              };
-          in { ${shell.name} = shell; };
+        mkVersionedPackages = hsVersion:
+          pkgs.lib.attrsets.mapAttrs' (name: _: {
+            name = "${name}-${hsVersion}";
+            value = pkgs.haskell.packages.${hsVersion}.${name};
+          }) pkgs.localHsPackages;
       in {
-        packages = mapcat localPackages hsVersions;
+        packages = pkgs.lib.mapcat mkVersionedPackages pkgs.hsVersions;
 
-        devShells = mapcat mkDevShell hsVersions;
+        devShells = pkgs.lib.mapcat pkgs.mkDevShell pkgs.hsVersions;
       });
 }
