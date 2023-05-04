b7b554848c143f449d513265b74059a0d149207c
Raghu Kaippully <rkaippully@gmail.com>
2023-05-04 21:29:59 +0530

    Upload using nix

---
+++ scripts/hackage-upload.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+
+# This script uploads packages to hackage
+
+set -euo pipefail
+
+HACKAGE_API_KEY=$1
+
+if [[ "$HACKAGE_API_KEY" == "" ]]; then
+    echo "Usage: $0 <hackage-api-key>"
+    exit 1
+fi
+
+upload_package() {
+    local archive=$1
+    curl --silent \
+         --header "Accept: text/plain" \
+         --header "Authorization: X-ApiKey $HACKAGE_API_KEY" \
+         --form "package=@$archive" \
+         https://hackage.haskell.org/packages/
+}
+
+cabal sdist all
+
+upload_package dist-newstyle/sdist/webgear-core-*.tar.gz
+upload_package dist-newstyle/sdist/webgear-server-*.tar.gz
+upload_package dist-newstyle/sdist/webgear-openapi-*.tar.gz
