5fcc4d58ec0822ce2840b9c64885bcbd89c6e1cc
Raghu Kaippully <rkaippully@gmail.com>
2023-12-17 16:11:28 +0530

    Change Body trait types
    - The 'Body' type now contains a single mime type instead of a
      list. This simplifies a lot of logic. We cab always chain
      requestBody middlewares to handle the case of multiple possible MIME
      types in requests.
    - Introduced UnknownContentBody when the body MIME type is not
      statically known.
    - No need to use Proxy values of MIME types anymore!

--- webgear-core/src/WebGear/Core/Trait/Body.hs
+++ webgear-core/src/WebGear/Core/Trait/Body.hs
@@ -16,6 +16,7 @@
 module WebGear.Core.Trait.Body (
   -- * Traits
   Body (..),
+  UnknownContentBody (..),
 
   -- * Middlewares
   requestBody,
@@ -24,32 +25,46 @@ module WebGear.Core.Trait.Body (
   setBodyWithoutContentType,
 ) where
 
-import Control.Arrow (ArrowChoice, (<<<))
+import Control.Arrow ((<<<))
 import Data.Kind (Type)
-import Data.Proxy (Proxy (..))
 import Data.Text (Text)
 import Data.Text.Encoding (decodeUtf8)
 import qualified Network.HTTP.Media as HTTP
 import qualified Network.HTTP.Types as HTTP
-import WebGear.Core.Handler (Handler, Middleware, unwitnessA)
+import WebGear.Core.Handler (Handler (..), Middleware, unwitnessA)
 import WebGear.Core.MIMEType (MIMEType (..))
 import WebGear.Core.Request (Request)
-import WebGear.Core.Response (Response)
-import WebGear.Core.Trait (Get, Set, Sets, Trait (..), TraitAbsence (..), With, plant, probe)
+import WebGear.Core.Response (Response, ResponseBody)
+import WebGear.Core.Trait (
+  Get,
+  Set,
+  Sets,
+  Trait (..),
+  TraitAbsence (..),
+  With (..),
+  plant,
+  probe,
+ )
 import WebGear.Core.Trait.Header (RequiredResponseHeader, ResponseHeader (..))
 import WebGear.Core.Trait.Status (Status, mkResponse)
 
--- | Request or response body with MIME types @mts@ and type @t@.
-data Body (mts :: [Type]) (t :: Type) = Body
+-- | Request or response body with MIME types @mimeTypes@ and type @t@.
+newtype Body (mimeType :: Type) (t :: Type) = Body mimeType
 
-instance Trait (Body mts t) Request where
-  type Attribute (Body mts t) Request = t
+instance Trait (Body mt t) Request where
+  type Attribute (Body mt t) Request = t
 
-instance TraitAbsence (Body mts t) Request where
-  type Absence (Body mts t) Request = Text
+instance TraitAbsence (Body mt t) Request where
+  type Absence (Body mt t) Request = Text
 
-instance Trait (Body mts t) Response where
-  type Attribute (Body mts t) Response = t
+instance Trait (Body mt t) Response where
+  type Attribute (Body mt t) Response = t
+
+-- | Type representing responses without a statically known MIME type
+data UnknownContentBody = UnknownContentBody
+
+instance Trait UnknownContentBody Response where
+  type Attribute UnknownContentBody Response = ResponseBody
 
 {- | Middleware to extract a request body.
 
@@ -59,17 +74,20 @@ instance Trait (Body mts t) Response where
  Usage:
 
 @
- requestBody \@'['PlainText'] \@'Text' errorHandler nextHandler
+ requestBody \@'Text' 'PlainText' errorHandler nextHandler
 @
 -}
 requestBody ::
-  forall mts t h req.
-  (Get h (Body mts t) Request, ArrowChoice h) =>
+  forall t mt h m ts.
+  ( Handler h m
+  , Get h (Body mt t) Request
+  ) =>
+  mt ->
   -- | Error handler in case body cannot be retrieved
-  h (Request `With` req, Text) Response ->
-  Middleware h req (Body mts t : req)
-requestBody errorHandler nextHandler = proc request -> do
-  result <- probe Body -< request
+  h (Request `With` ts, Text) Response ->
+  Middleware h ts (Body mt t : ts)
+requestBody mt errorHandler nextHandler = proc request -> do
+  result <- probe (Body mt) -< request
   case result of
     Left err -> errorHandler -< (request, err)
     Right t -> nextHandler -< t
@@ -84,17 +102,20 @@ requestBody errorHandler nextHandler = proc request -> do
 
 @
  let body :: SomeJSONType = ...
- response' <- setBody \@'JSON' -< (response, body)
+ response' <- setBody 'JSON' -< (response, body)
 @
 -}
 setBody ::
-  forall mt body h res.
-  (Sets h [Body '[mt] body, RequiredResponseHeader "Content-Type" Text] Response, MIMEType mt) =>
-  h (Response `With` res, body) (Response `With` (RequiredResponseHeader "Content-Type" Text : Body '[mt] body : res))
-setBody = proc (response, body) -> do
-  response' <- plant Body -< (response, body)
-  let mt = decodeUtf8 $ HTTP.renderHeader $ mimeType $ Proxy @mt
-  plant ResponseHeader -< (response', mt)
+  forall body mt h ts.
+  ( Sets h [Body mt body, RequiredResponseHeader "Content-Type" Text] Response
+  , MIMEType mt
+  ) =>
+  mt ->
+  h (Response `With` ts, body) (Response `With` (Body mt body : RequiredResponseHeader "Content-Type" Text : ts))
+setBody mt = proc (response, body) -> do
+  let ct = mimeType mt
+  response' <- plant ResponseHeader -< (response, decodeUtf8 $ HTTP.renderHeader ct)
+  plant (Body mt) -< (response', body)
 {-# INLINE setBody #-}
 
 {- | Set the response body without specifying any media type.
@@ -102,15 +123,15 @@ setBody = proc (response, body) -> do
 Usage:
 
 @
- let body :: SomeType = ...
+ let body :: ResponseBody = ...
  response' <- setBodyWithoutContentType -< (response, body)
 @
 -}
 setBodyWithoutContentType ::
-  forall body h res.
-  (Set h (Body '[] body) Response) =>
-  h (Response `With` res, body) (Response `With` (Body '[] body : res))
-setBodyWithoutContentType = plant Body
+  forall h res.
+  (Set h UnknownContentBody Response) =>
+  h (Response `With` res, ResponseBody) (Response `With` (UnknownContentBody : res))
+setBodyWithoutContentType = plant UnknownContentBody
 {-# INLINE setBodyWithoutContentType #-}
 
 {- | A convenience arrow to generate a response specifying a status and body.
@@ -121,20 +142,20 @@ setBodyWithoutContentType = plant Body
 
 @
  let body :: SomeJSONType = ...
- respondA 'HTTP.ok200' (Proxy \@'JSON') -< body
+ respondA 'HTTP.ok200' 'JSON' -< body
 @
 -}
 respondA ::
-  forall mt body h m.
+  forall body mt h m.
   ( Handler h m
-  , Sets h [Status, Body '[mt] body, RequiredResponseHeader "Content-Type" Text] Response
+  , Sets h [Status, Body mt body, RequiredResponseHeader "Content-Type" Text] Response
   , MIMEType mt
   ) =>
   -- | Response status
   HTTP.Status ->
-  Proxy mt ->
+  mt ->
   h body Response
-respondA status _ = proc body -> do
+respondA status mt = proc body -> do
   response <- mkResponse status -< ()
-  unwitnessA <<< setBody @mt -< (response, body)
+  unwitnessA <<< setBody mt -< (response, body)
 {-# INLINE respondA #-}
