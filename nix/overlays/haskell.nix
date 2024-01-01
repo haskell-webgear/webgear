390496b5e42ea9057c37aaf3992eb0065daa9d9e
Raghu Kaippully <rkaippully@gmail.com>
2024-01-01 16:34:08 +0530

    Rollback ghc-9.8
    Just too many dependencies are broken with ghc-9.8. We'll try it again
    later.

--- nix/overlays/haskell.nix
+++ nix/overlays/haskell.nix
@@ -6,8 +6,8 @@ let
   hsLib = final.haskell.lib.compose;
   mapcat = f: lst: builtins.foldl' (l: r: l // r) {} (map f lst);
 
-  ghcVersions = ["981" "963" "948" "928" "902" "8107"];
-  defaultGHCVersion = "981";
+  ghcVersions = ["963" "948" "928" "902" "8107"];
+  defaultGHCVersion = "963";
 
   localHsPackages = {
     # Libraries
@@ -34,34 +34,6 @@ let
 
   haskell = prev.haskell // {
     packages = prev.haskell.packages // {
-      ghc981 = prev.haskell.packages.ghc981.override {
-        overrides = hfinal: hprev:
-          final.lib.mapAttrs (mkLocalDerivation hfinal) localHsPackages // {
-            # Need specific versions for benchmarking
-            scotty = hsLib.dontHaddock (hfinal.callPackage ../haskell-packages/scotty.nix {});
-            servant = hsLib.dontHaddock (hfinal.callPackage ../haskell-packages/servant.nix {});
-            servant-server = hsLib.dontHaddock (hfinal.callPackage ../haskell-packages/servant-server.nix {});
-
-            # For ghc-9.8
-            system-cxx-std-lib = null;
-            aeson = hprev.aeson_2_2_1_0;
-            bifunctors = hprev.bifunctors_5_6_1;
-            free = hprev.free_5_2;
-            generics-sop = hprev.generics-sop_0_5_1_4;
-            hspec-core = hprev.hspec-core_2_11_7;
-            hspec-meta = hprev.hspec-meta_2_11_7;
-            some = hprev.some_1_0_6;
-            tagged = hprev.tagged_0_8_8;
-            th-abstraction = hprev.th-abstraction_0_6_0_0;
-
-            # No longer broken
-            bytestring-conversion = hsLib.unmarkBroken hprev.bytestring-conversion;
-
-            # doctests fail
-            openapi3 = hsLib.dontCheck (hsLib.unmarkBroken hprev.openapi3);
-          };
-      };
-
       ghc963 = prev.haskell.packages.ghc963.override {
         overrides = hfinal: hprev:
           final.lib.mapAttrs (mkLocalDerivation hfinal) localHsPackages // {
