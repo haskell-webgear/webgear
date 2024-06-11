b26970fadcf77e6c19a205111e124d9891a102b0
Raghu Kaippully <rkaippully@gmail.com>
2024-06-11 13:44:58 +0530

    Reimplement swagger internals
    The new implementation removes Tree, DocNode, and CompactDocNode
    intermediate representations and directly manipulates OpenApi
    records. This is a cleaner solution.

--- webgear-swagger/src/WebGear/Swagger/Trait/Auth.hs
+++ webgear-swagger/src/WebGear/Swagger/Trait/Auth.hs
@@ -1,18 +1,42 @@
 {-# OPTIONS_GHC -Wno-orphans #-}
 
 -- | Functions and instances for authentication
-module WebGear.Swagger.Trait.Auth () where
+module WebGear.Swagger.Trait.Auth (addSecurityScheme) where
 
+import Control.Lens ((&), (.~), (<>~))
+import Control.Monad.State.Strict (MonadState)
 import Data.Proxy (Proxy (..))
 import Data.Swagger (
   Definitions,
   NamedSchema,
   Schema,
+  SecurityDefinitions (..),
+  SecurityRequirement (..),
+  SecurityScheme,
+  Swagger,
   ToSchema (..),
+  allOperations,
+  description,
+  security,
+  securityDefinitions,
  )
 import Data.Swagger.Declare (Declare)
+import Data.Text (Text)
+import WebGear.Core.Handler (Description (..))
 import WebGear.Core.Trait.Auth.Common (AuthToken)
+import WebGear.Swagger.Handler (Documentation (..), consumeDescription)
 
 instance ToSchema (AuthToken scheme) where
   declareNamedSchema :: Proxy (AuthToken scheme) -> Declare (Definitions Schema) NamedSchema
   declareNamedSchema _ = declareNamedSchema $ Proxy @String
+
+addSecurityScheme :: (MonadState Documentation m) => Text -> SecurityScheme -> Swagger -> m Swagger
+addSecurityScheme schemeName scheme doc = do
+  desc <- consumeDescription
+  let scheme' = scheme & description .~ fmap getDescription desc
+      secDefs = SecurityDefinitions [(schemeName, scheme')]
+      secReqs = [SecurityRequirement [(schemeName, [])]] :: [SecurityRequirement]
+  pure $
+    doc
+      & securityDefinitions <>~ secDefs
+      & allOperations . security <>~ secReqs
--- webgear-swagger/src/WebGear/Swagger/Trait/Cookie.hs
+++ webgear-swagger/src/WebGear/Swagger/Trait/Cookie.hs
@@ -1,28 +1,26 @@
 {-# OPTIONS_GHC -Wno-orphans #-}
 
-{- | Swagger implementation of 'WG.Cookie' and 'WG.SetCookie' traits.
-
-Swagger 2.0 does not support cookie authentication. So these
-implementations are no-ops.
--}
+-- | Swagger implementation of 'WG.Cookie' and 'WG.SetCookie' traits.
 module WebGear.Swagger.Trait.Cookie () where
 
 import WebGear.Core.Request (Request)
 import WebGear.Core.Response (Response)
 import WebGear.Core.Trait (Get (..), Set (..), Trait, TraitAbsence)
 import qualified WebGear.Core.Trait.Cookie as WG
-import WebGear.Swagger.Handler (SwaggerHandler (..), nullNode)
+import WebGear.Swagger.Handler (SwaggerHandler (..))
+
+-- Cookie information is not captured by Swagger
 
 instance
   (TraitAbsence (WG.Cookie e name val) Request) =>
   Get (SwaggerHandler m) (WG.Cookie e name val) Request
   where
   {-# INLINE getTrait #-}
-  getTrait WG.Cookie = SwaggerHandler nullNode
+  getTrait WG.Cookie = SwaggerHandler pure
 
 instance
   (Trait (WG.SetCookie e name) Response) =>
   Set (SwaggerHandler m) (WG.SetCookie e name) Response
   where
   {-# INLINE setTrait #-}
-  setTrait WG.SetCookie _ = SwaggerHandler nullNode
+  setTrait WG.SetCookie _ = SwaggerHandler pure
